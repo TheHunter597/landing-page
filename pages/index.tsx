b72625be1d918a5801f495edfa4dc7fd26faea14
TheHunter597 <thehunter597777@gmail.com>
2022-08-30 13:14:03 -0700

    fixing the bug that resulted from me fixing the bug before it

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import Header from "../components/Home/Header/Header";
 import FirstSection from "../components/Home/firstSection/FirstSection";
 import SecondSection from "../components/Home/secondSection/SecondSection";
 import ThirdSection from "../components/Home/thirdSection/ThirdSection";
-import Footer from "../components/Home/Footer/footer";
+import Footer from "../components/Home/footer/footer";
 
 function Home() {
   return (
