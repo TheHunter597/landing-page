0c74bd5b43897bddadabbea34904dc1b69400bd7
TheHunter597 <thehunter597777@gmail.com>
2022-08-30 13:15:13 -0700

    type f for respect

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import Header from "../components/Home/Header/Header";
 import FirstSection from "../components/Home/firstSection/FirstSection";
 import SecondSection from "../components/Home/secondSection/SecondSection";
 import ThirdSection from "../components/Home/thirdSection/ThirdSection";
-import Footer from "../components/Home/footer/footer";
+import Footer from "../components/Home/footer/Footer";
 
 function Home() {
   return (
